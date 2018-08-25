f7cf178ec9576d9f553eb281a417a4af5977b57f
Valery <con-bo@yandex.ru>
2018-08-25 19:11:47 +0300

    add an appcompat version of the injection manager

---
+++ appcompat/src/main/java/me/vponomarenko/injectionmanager/support/CompatActivityLifecycleHelper.kt
@@ -0,0 +1,48 @@
+package me.vponomarenko.injectionmanager.support
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.vponomarenko.injectionmanager.IHasComponent
+import me.vponomarenko.injectionmanager.callbacks.IRemoveComponentCallback
+
+/**
+ * Author: Valery Ponomarenko
+ * Date: 25/08/2018
+ * LinkedIn: https://www.linkedin.com/in/ponomarenkovalery
+ */
+
+internal class CompatActivityLifecycleHelper(
+    private val removeComponentCallback: IRemoveComponentCallback
+) : Application.ActivityLifecycleCallbacks {
+    override fun onActivityPaused(activity: Activity) {
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        if (activity is IHasComponent && activity.isFinishing) {
+            removeComponentCallback.onRemove(activity.getComponentKey())
+        }
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+    }
+
+    override fun onActivityCreated(activity: Activity, outState: Bundle?) {
+        if (activity is AppCompatActivity) {
+            activity.supportFragmentManager.registerFragmentLifecycleCallbacks(
+                CompatFragmentLifecycleHelper(removeComponentCallback),
+                true
+            )
+        }
+    }
+}
---
+++ appcompat/src/main/java/me/vponomarenko/injectionmanager/support/CompatFragmentLifecycleHelper.kt
@@ -0,0 +1,59 @@
+package me.vponomarenko.injectionmanager.support
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import me.vponomarenko.injectionmanager.IHasComponent
+import me.vponomarenko.injectionmanager.callbacks.IRemoveComponentCallback
+
+/**
+ * Author: Valery Ponomarenko
+ * Date: 25/08/2018
+ * LinkedIn: https://www.linkedin.com/in/ponomarenkovalery
+ */
+
+class CompatFragmentLifecycleHelper(
+    private val removeComponentCallback: IRemoveComponentCallback
+) : FragmentManager.FragmentLifecycleCallbacks() {
+    private var isInSaveState = false
+
+    override fun onFragmentStarted(fm: FragmentManager, f: Fragment) {
+        super.onFragmentStarted(fm, f)
+        isInSaveState = false
+    }
+
+    override fun onFragmentResumed(fm: FragmentManager, f: Fragment) {
+        super.onFragmentResumed(fm, f)
+        isInSaveState = false
+    }
+
+    override fun onFragmentSaveInstanceState(fm: FragmentManager, f: Fragment, outState: Bundle) {
+        super.onFragmentSaveInstanceState(fm, f, outState)
+        isInSaveState = true
+    }
+
+    override fun onFragmentDestroyed(fm: FragmentManager, f: Fragment) {
+        super.onFragmentDestroyed(fm, f)
+        if (f !is IHasComponent) return
+
+        if (f.requireActivity().isFinishing) {
+            removeComponentCallback.onRemove(f.getComponentKey())
+            return
+        }
+
+        if (isInSaveState) {
+            isInSaveState = false
+            return
+        }
+
+        var anyParentIsRemoving = false
+        var parent = f.parentFragment
+        while (!anyParentIsRemoving && parent != null) {
+            anyParentIsRemoving = parent.isRemoving
+            parent = parent.parentFragment
+        }
+        if (f.isRemoving || anyParentIsRemoving) {
+            removeComponentCallback.onRemove(f.getComponentKey())
+        }
+    }
+}
